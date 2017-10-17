edb54df3144767a0d1fe90e02084aefff0251da5
DESKTOP-39ABF47\Hong Zheng <zh_1991@163.com>
2017-10-17 09:41:21 +0800

    The Hero Editor done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,11 @@ import { Component } from '@angular/core';
   template: `<h1>{{title}}</h1><h2>{{hero.name}} 
     {{hero.id}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
-    <div><label>name: </label>{{hero.name}}</div>`,
+    <div><label>name: </label>{{hero.name}}</div>
+    <div>
+    <label>name: </label>
+    <input [(ngModel)]="hero.name" placeholder="name">
+    </div>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
@@ -17,9 +21,4 @@ export class AppComponent {
     name: "Windstorm",
   };
 
-  arr: string[] = new Array();
-  
-  sss(): void {
-    this.arr.sort();
-  }
 }
